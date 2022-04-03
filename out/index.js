935d23b4c08ea7c2c605c7ffd9022b3cde4661da
Adri Haik Martos <ady642@gmail.com>
2022-04-03 22:34:52 +0200

    remove out folder

--- out/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UnitTestFactory_1 = require("./templates/UnitTestFactory");
-const fs = require("fs");
-const path = './dummy_data/src/Common/components/Buttons/NattoButton.vue';
-const data = fs.readFileSync(path, 'utf8');
-const unitTestFactory = new UnitTestFactory_1.default(path, data);
-unitTestFactory.lintTestSuites().then((res) => console.log(res));
-//# sourceMappingURL=index.js.map
